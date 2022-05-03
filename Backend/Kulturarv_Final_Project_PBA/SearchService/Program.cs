ecd3b13062afbb56e980bf39c464845ee59d10c1
Magnus Just Sund Carlsen <1074203@ucn.dk>
2022-05-03 14:43:09 +0200

    Added developer branch. This is the one to be worked on

---
+++ Backend/Kulturarv_Final_Project_PBA/SearchService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SearchService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
