6471c8f124a47a81ae6fd16b9e41866fcfeb0f07
Magnus Just Sund Carlsen <carlsen57@gmail.com>
2022-05-05 11:50:17 +0200

    Added methods, comments and TODO for the rest of the script

--- RPA's/Script/Program.cs
+++ RPA's/Script/Program.cs
@@ -1,11 +1,17 @@
-﻿using Newtonsoft.Json;
-using System;
+﻿using System;
 using System.Net.Http;
-using System.Text.Json;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions; 
 
 namespace Script
 {
+    //https://wiki.archlinux.org/title/.NET
+    //Segmentation Fault Problem Solution --> Use pacman to install .NET Core SDK and Runtime.
+
+    //TODO: Should be able to restart or pause for a certain time. 
+    //Should be able to add search parameter.
+    //Run each search in its own thread.  
+
     class Program
     {
         static async Task Main(string[] args)
@@ -26,7 +32,7 @@ namespace Script
                     string contentString = await response.Content.ReadAsStringAsync();
                     Console.WriteLine(contentString);
 
-                    Strip_Text_From_HTML_RegEx(contentString); 
+                    Strip_Text_From_HTML_RegEx_Kulturarv(contentString); 
                 }   
                 else {
                     Search_Point = 1; 
@@ -34,11 +40,40 @@ namespace Script
             }
         }
 
-        private static void Strip_Text_From_HTML_RegEx(string data){
+        private async static Task Search_Europeana_Database()
+        {
+            string Host = ""; 
+            string Path = ""; 
+            int Search_Point = 1; 
+            int Max_Search_Point = 3000; 
+            HttpClient client = new HttpClient(); 
+            while(true)
+            {
+                if(Search_Point <= Max_Search_Point)
+                {
+                    string uri = Host + Path + ""; 
+                    HttpResponseMessage response = await client.GetAsync(uri);
+                    string contentString = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(contentString);
+
+                    Strip_Text_From_HTML_RegEx_Kulturarv(contentString);
+                }
+            }
+
+        }
+        private static void Cleanup_And_Prepare_Europeana_Data_For_Database(string data)
+        {
+            //TODO: Recognize data. 
+            //Validate data. --> Language, content and maybe other missing parameters. 
+            string result = ""; 
+            Save_Result_To_File(result); 
+        }
+
+        private static void Strip_Text_From_HTML_RegEx_Kulturarv(string data){
             string RegEx = ""; 
             string result = ""; //List?
 
-            //DO THE WORK HERE!
+            Regex re = new Regex(RegEx, RegexOptions.IgnoreCase); 
 
             Save_Result_To_File(result); 
         }
