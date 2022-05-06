5789de2de3aab51e4f38f7b307f26416f19b4407
Magnus Just Sund Carlsen <1074203@ucn.dk>
2022-05-06 14:19:25 +0200

    Stuff

--- RPA's/Script/Program.cs
+++ RPA's/Script/Program.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Net.Http;
 using System.Threading.Tasks;
-using System.Text.RegularExpressions; 
+using System.Text.RegularExpressions;
+using Neo4j.Driver;
 
 namespace Script
 {
@@ -12,11 +13,17 @@ namespace Script
     //Should be able to add search parameter.
     //Run each search in its own thread.  
 
-    class Program
+    public class Program
     {
-        private static string KulturArv_Location = args[0];
-        static async Task Main(string[] args)
+        private static string KulturArv_Location;       
+        private static IDriver _driver;
+        private static string uri = ""; //Neo4J IP
+        static async Task Main(string[] args)   //Kommune = 0, Password = 1
         {
+            KulturArv_Location = args[0];
+            string user = args[1];
+            string password = args[2];
+            _driver = GraphDatabase.Driver(uri, AuthTokens.Basic(user, password));
             while(true)
             {
             await SearchAsync();
@@ -37,17 +44,16 @@ namespace Script
                     string contentString = await response.Content.ReadAsStringAsync();
                     Console.WriteLine(contentString);
 
-                    Strip_Text_From_HTML_RegEx_Kulturarv(contentString); 
+                    await Strip_Text_From_HTML_RegEx_KulturarvAsync(contentString); 
                 }   
                 else {
                     Search_Point = 1; 
                 }
-            
         }
 
         private async static Task Search_Europeana_Database()
         {
-            string Host = ""; 
+            string Host = "";   //TODO: Mangler. EDM --> Undersøg. 
             string Path = ""; 
             int Search_Point = 1; 
             int Max_Search_Point = 3000; 
@@ -60,33 +66,42 @@ namespace Script
                     string contentString = await response.Content.ReadAsStringAsync();
                     Console.WriteLine(contentString);
 
-                    Strip_Text_From_HTML_RegEx_Kulturarv(contentString);
+                    await Strip_Text_From_HTML_RegEx_KulturarvAsync(contentString);
                 }
             
 
         }
 
-        private static void Cleanup_And_Prepare_Europeana_Data_For_Database(string data)
+        private static async Task Cleanup_And_Prepare_Europeana_Data_For_DatabaseAsync(string data)
         {
             //TODO: Recognize data. 
             //Validate data. --> Language, content and maybe other missing parameters. 
             string result = ""; 
-            Save_Result_To_File(result); 
+            await Save_Result_To_Neo4J(result); 
         }
 
-        private static void Strip_Text_From_HTML_RegEx_Kulturarv(string data){
-            string RegEx = ""; 
-            string result = ""; //List?
-
-            Regex re = new Regex(RegEx, RegexOptions.IgnoreCase); 
-
-            Save_Result_To_File(result); 
+        private static async Task Strip_Text_From_HTML_RegEx_KulturarvAsync(string data){
+            string RegEx = "(<!-- Start free text for location -->)"; 
+            string Result = "";
+            Regex re = new Regex(RegEx, RegexOptions.None);
+            Result = re.ToString();  
+            await Save_Result_To_Neo4J(Result); 
         }
 
         //This should be changed to save directly to the database (Neo4J).
-        private static void Save_Result_To_File(string document){
-            string Save_Path = @"C:\Users\Magnus Carlsen\Desktop\Script";
-            System.IO.File.WriteAllText(Save_Path, document);
+        private static async Task Save_Result_To_Neo4J(string document){
+            using(var session = _driver.AsyncSession())
+            {
+                var return_message = session.WriteTransactionAsync(x => 
+                {
+                var result = x.RunAsync("CREATE (a:Greeting) " +
+                                    "SET a.message = $message " +
+                                    "RETURN a.message + ', from node ' + id(a)",
+                    new {document});
+                    return result.Single()[0].As<string>(); 
+                });
+                Console.WriteLine(return_message); 
+            }
         }   
     }
 }
