7eae55584ae875461c735d0a21a4a454c32bd96c
Magnus Just Sund Carlsen <1074203@ucn.dk>
2022-05-11 15:55:02 +0200

    ressources added

--- Backend/Kulturarv_Final_Project_PBA/SearchService/Program.cs
+++ Backend/Kulturarv_Final_Project_PBA/SearchService/Program.cs
@@ -2,6 +2,10 @@
 
 namespace SearchService
 {
+    #region Ressources
+        //CQRS: https://www.eventstore.com/cqrs-pattern
+        //RabbitMQ: https://www.rabbitmq.com/getstarted.html 
+    #endregion
     class Program
     {
         static void Main(string[] args)
