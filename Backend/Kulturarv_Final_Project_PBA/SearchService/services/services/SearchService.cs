d475488aebf2cae9d97e9de65775a9015df8cfb4
Magnus Just Sund Carlsen <1074203@ucn.dk>
2022-05-12 10:35:50 +0200

    Search service interface methods added

--- Backend/Kulturarv_Final_Project_PBA/SearchService/services/services/SearchService.cs
+++ Backend/Kulturarv_Final_Project_PBA/SearchService/services/services/SearchService.cs
@@ -8,4 +8,29 @@ public class Search_Service : Search_Service_IF
     {
         Console.WriteLine("Search Service object init"); 
     }
+
+    public void Search_By_Heritage_Type()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Search_By_Institution_Event()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Search_By_Region()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Search_By_Time_Age()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Search_By_Top_10_Close_Heritage_Sites()
+    {
+        throw new NotImplementedException();
+    }
 }
