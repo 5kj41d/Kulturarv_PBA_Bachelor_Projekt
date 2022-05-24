1f5079517d5145c83ddbd31e69d452587c071c24
Magnus Just Sund Carlsen <1074203@ucn.dk>
2022-05-24 11:14:43 +0200

    Yes

--- Backend_To_Frontend/Gateways/Mobile_Gateway_Test_Project/Message_Sending_Tests.cs
+++ Backend_To_Frontend/Gateways/Mobile_Gateway_Test_Project/Message_Sending_Tests.cs
@@ -14,24 +14,17 @@ namespace Mobile_Gateway_Test_Project
 {
     public class TestsFixture : IDisposable
     {
-        public readonly ILogger<Message_Sending_Tests> _logger;
-        public readonly IOptions<RabbitMqConfiguration> _configuration;
-        public IConfiguration Configuration { get; }
         private readonly TestServer server;
         private readonly HttpClient client;
-        
-        public TestsFixture(ILogger<Message_Sending_Tests> logger, IOptions<RabbitMqConfiguration> options, IConfiguration configuration)
+
+        public TestsFixture()
         {
             // Do "global" initialization here; Only called once.
             server = new TestServer(new WebHostBuilder().UseStartup<Startup>());
             client = server.CreateClient();
-            IServiceCollection services = new ServiceCollection(); 
-            _logger = logger; 
-            _configuration = options;
-            Configuration = configuration; 
         }
 
-        public IServiceProvider ServiceProvider => server.Host.Services;    
+        public IServiceProvider ServiceProvider => server.Host.Services;
 
         public void Dispose()
         {
@@ -58,7 +51,7 @@ namespace Mobile_Gateway_Test_Project
         private void ConfigureServices(IServiceCollection services)
         {
             //Add Logging DI service:
-            var serviceProvider = services.BuildServiceProvider(); 
+            var serviceProvider = services.BuildServiceProvider();
             var logger = serviceProvider.GetService<ILogger<Message_Sending_Tests>>();
             services.AddSingleton(typeof(ILogger), logger);
             //RabbitMQ configuration.
@@ -72,14 +65,17 @@ namespace Mobile_Gateway_Test_Project
     public class Message_Sending_Tests : IClassFixture<TestsFixture>
     {
         #region Version 1 tests for search service controller. --> RabbitMQ Classes for now.
-       private const string Routing_key = "Search";
-       private readonly TestsFixture _testsFixture; 
-       private readonly ILogger<Message_Sending_Tests> _logger; 
-       public Message_Sending_Tests(TestsFixture testsFixture, ILogger<Message_Sending_Tests> logger)
-       {
-           _testsFixture = testsFixture; //Get data from testsfixture. 
-            _logger = logger; 
-       }
+        private const string Routing_key = "Search";
+        private readonly TestsFixture _testsFixture;
+        private readonly ILogger<Message_Sending_Tests> _logger;
+        public readonly IOptions<RabbitMqConfiguration> _configuration;
+
+        public Message_Sending_Tests(TestsFixture testsFixture, ILogger<Message_Sending_Tests> logger, IOptions<RabbitMqConfiguration> options)
+        {
+            _testsFixture = testsFixture; //Get data from testsfixture. 
+            _logger = logger;
+            _configuration = options;
+        }
 
         ///////////////////// Tests starts /////////////////////
         [Fact]
@@ -99,7 +95,7 @@ namespace Mobile_Gateway_Test_Project
                     .Sent_Message_To_Message_Bus_RPC(sent_Model);
 
             //Assert
-            Assert.NotNull (result);
+            Assert.NotNull(result);
         }
 
         [Fact]
@@ -119,7 +115,7 @@ namespace Mobile_Gateway_Test_Project
                     .Sent_Message_To_Message_Bus_RPC(sent_Model);
 
             //Assert
-            Assert.NotNull (result);
+            Assert.NotNull(result);
         }
 
         [Fact]
@@ -139,7 +135,7 @@ namespace Mobile_Gateway_Test_Project
                     .Sent_Message_To_Message_Bus_RPC(sent_Model);
 
             //Assert
-            Assert.NotNull (result);
+            Assert.NotNull(result);
         }
 
         [Fact]
@@ -159,7 +155,7 @@ namespace Mobile_Gateway_Test_Project
                     .Sent_Message_To_Message_Bus_RPC(sent_Model);
 
             //Assert
-            Assert.NotNull (result);
+            Assert.NotNull(result);
         }
 
         [Fact]
@@ -206,11 +202,11 @@ namespace Mobile_Gateway_Test_Project
         public void Test_Rpc_Connection_To_RabbitMQ()
         {
             //TODO: get configs from TestFixture. 
-            Rpc_sender rpc_Sender = new Rpc_sender(_testsFixture._configuration, _testsFixture._logger);
-            var result = rpc_Sender.Test_Connection();  
+            Rpc_sender rpc_Sender = new Rpc_sender(_configuration, _logger);
+            var result = rpc_Sender.Test_Connection();
             Assert.True(result);
         }
         ///////////////////// Tests end /////////////////////
-    #endregion
+        #endregion
     }
 }
