7264a2415f1397b863c9b08ca807b9f48922a326
Magnus Just Sund Carlsen <1074203@ucn.dk>
2022-05-24 14:22:44 +0200

    RabbitMQ method listenandrespond added

--- Backend/Kulturarv_Final_Project_PBA/SearchService/rabbitmq/rpc_search_handler/Search_Rpc.cs
+++ Backend/Kulturarv_Final_Project_PBA/SearchService/rabbitmq/rpc_search_handler/Search_Rpc.cs
@@ -1,74 +1,63 @@
 using System;
-using System.Collections.Concurrent;
-using System.Text;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
+using System.Text;
 
-public class Search_Rpc
+public class RPCServer 
 {
-	private readonly IConnection connection;
-    private readonly IModel channel;
-    private string replyQueueName;
-    private EventingBasicConsumer consumer;
-    private readonly BlockingCollection<string> respQueue = new BlockingCollection<string>();
-    private IBasicProperties props;
-	public Search_Rpc()
+	public RPCServer()
 	{
-		Init(); 
+		Listen_And_Respond(); 
 	}
-	
-	private void Init()
-	{
-		Console.WriteLine("Search_Rpc init!");
-		var factory = new ConnectionFactory();	//TODO: --> Need URL.
-		using(var connection = factory.CreateConnection())
-		{
-			using(var channel = connection.CreateModel())
-			{
-				replyQueueName = channel.QueueDeclare().QueueName; 
-				consumer = new EventingBasicConsumer(channel);
+    public void Listen_And_Respond()
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            channel.QueueDeclare(queue: "rpc_queue", durable: false,
+              exclusive: false, autoDelete: false, arguments: null);
+            channel.BasicQos(0, 1, false);
+            var consumer = new EventingBasicConsumer(channel);
+            channel.BasicConsume(queue: "rpc_queue",
+              autoAck: false, consumer: consumer);
+            Console.WriteLine(" [x] Awaiting RPC requests");
 
-				//NOTE: Should this be done in the gateways?
-				props = channel.CreateBasicProperties();
-        		var correlationId = Guid.NewGuid().ToString();
-        		props.CorrelationId = correlationId;
-        		props.ReplyTo = replyQueueName;
+            consumer.Received += (model, ea) =>
+            {
+                string response = null;
 
-				consumer.Received += (model, ea) =>
-        	{
-            	var body = ea.Body.ToArray();
-            	var response = Encoding.UTF8.GetString(body);
-            	if (ea.BasicProperties.CorrelationId == correlationId)
-            	{
-                	respQueue.Add(response);
-            	}
-        	};
+                var body = ea.Body.ToArray();
+                var props = ea.BasicProperties;
+                var replyProps = channel.CreateBasicProperties();
+                replyProps.CorrelationId = props.CorrelationId;
 
-        	channel.BasicConsume(
-            	consumer: consumer,
-            	queue: replyQueueName,
-            	autoAck: true);
-			}
+                try
+                {
+                    var message = Encoding.UTF8.GetString(body);
+                    //int n = int.Parse(message);
+                    //Console.WriteLine(" [.] fib({0})", message);
+                    //response = fib(n).ToString(); --> Method of our choice. 
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(" [.] " + e.Message);
+                    response = "";
+                }
+                finally
+                {
+                    var responseBytes = Encoding.UTF8.GetBytes(response);
+                    channel.BasicPublish(exchange: "", routingKey: props.ReplyTo,
+                      basicProperties: replyProps, body: responseBytes);
+                    channel.BasicAck(deliveryTag: ea.DeliveryTag,
+                      multiple: false);
+                }
+            };
 		}
-
 	}
 
-	//TODO: Should be called?
-	public string Call(string message)
-    {
-        var messageBytes = Encoding.UTF8.GetBytes(message);
-        channel.BasicPublish(
-            exchange: "",
-            routingKey: "rpc_queue",
-            basicProperties: props,
-            body: messageBytes);
-
-        return respQueue.Take();
-    }
-
-    public void Close()
-    {
-        connection.Close();
-    }
-	
+	private void Check_Message_Body()
+	{
+		//TODO: Check hvilken metode den skal kalde. 
+	}
 }
