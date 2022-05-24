ed986f7398f660292fc4b45637dae04c73bfef0f
Magnus Just Sund Carlsen <1074203@ucn.dk>
2022-05-24 14:27:22 +0200

    Removed app.xml

--- Backend/Kulturarv_Final_Project_PBA/SearchService/Program.cs
+++ Backend/Kulturarv_Final_Project_PBA/SearchService/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 using RabbitMQ.Client; 
 
 namespace SearchService
@@ -11,10 +12,11 @@ namespace SearchService
     #endregion
     public class Program
     {
-
-
         static async Task Main(string[] args)
         {
+            IConfiguration Config = new ConfigurationBuilder()
+                .AddJsonFile("appSettings.json")
+                .Build();
             //Search_Rpc search_Rpc = new Search_Rpc();
             Search_Service_IF searchService = new Search_Service();
             await searchService.Search_By_Top_10_Close_Heritage_Sites(9.003, 52.0005);
