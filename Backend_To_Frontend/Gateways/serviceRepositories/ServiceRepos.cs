6aae9481b14d412dece2162f1e322efc991805db
Magnus Just Sund Carlsen <carlsen57@gmail.com>
2022-06-20 11:09:20 +0200

    Added service repositories

---
+++ Backend_To_Frontend/Gateways/serviceRepositories/ServiceRepos.cs
@@ -0,0 +1,12 @@
+using System; 
+
+namespace serviceRepositories
+{
+    public class ServiceRepos : ServiceReposIF
+    {
+        public ServiceRepos()
+        {
+            
+        }
+    }
+}
