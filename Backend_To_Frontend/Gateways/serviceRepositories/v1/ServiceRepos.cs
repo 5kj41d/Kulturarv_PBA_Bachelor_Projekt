45014ef2b3dc0a756e533827356fafbf156863e2
Magnus Just Sund Carlsen <carlsen57@gmail.com>
2022-06-20 14:02:26 +0200

    Removed service repos - unessesary

--- Backend_To_Frontend/Gateways/serviceRepositories/v1/ServiceRepos.cs
+++
@@ -1,9 +0,0 @@
-using System; 
-
-namespace serviceRepositories
-{
-    public static class ServiceRepos : ServiceReposIF
-    {
-        
-    }
-}
--- Backend_To_Frontend/Gateways/serviceRepositories/v1/ServiceReposIF.cs
+++
@@ -1,9 +0,0 @@
-using System; 
-
-namespace serviceRepositories
-{
-    public class ServiceReposIF
-    {
-        
-    }
-}
