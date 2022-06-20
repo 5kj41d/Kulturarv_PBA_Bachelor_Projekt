1824e1d5896bdd9702b18ff957d5554a73454f23
Magnus Just Sund Carlsen <carlsen57@gmail.com>
2022-06-20 14:25:45 +0200

    Mobile aggregator init

---
+++ Backend_To_Frontend/Aggregators/MobileAggregator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MobileAggregator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
