a85ffd833e06845fa4681181d68bbe2f0b908f89
Magnus Just Sund Carlsen <carlsen57@gmail.com>
2022-06-20 17:22:06 +0200

    RabbitMq init

--- Backend_To_Frontend/Aggregators/MobileAggregator/rabbitmq/RabbitMqRPC.cs
+++ Backend_To_Frontend/Aggregators/MobileAggregator/rabbitmq/RabbitMqRPC.cs
@@ -1,6 +1,7 @@
 using System;
 using RabbitMQ.Client;
-using System.Text; 
+using System.Text;
+using RabbitMQ.Client.Events;
 
 namespace rabbitmq
 {
@@ -17,7 +18,51 @@ namespace rabbitmq
 
         private void Init()
         {
-            
+            var factory = new ConnectionFactory() {HostName = ""}; 
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "RPC_Request_Search_Queue", durable: true,
+                exclusive: false, autoDelete: false, arguments: null);
+                channel.BasicQos(0,1,false);
+                var consumer = new EventingBasicConsumer(channel);
+                channel.BasicConsume(queue: "RPC_Reply_Search_Queue", autoAck: false, consumer: consumer);
+                Console.WriteLine(" [x] Awaiting RPC requests");
+
+                consumer.Received += (model, ea) => 
+                {
+                    string response = null; 
+                    var body = ea.Body.ToArray(); 
+                    var props = ea.BasicProperties; 
+                    var replyProps = channel.CreateBasicProperties(); 
+                    replyProps.CorrelationId = props.CorrelationId; 
+
+                    try
+                    {
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine($"Message: {message}");  
+                        response = Make_Request_And_Get_Response(message);
+                    } 
+                    catch(Exception e) 
+                    {
+                        //TODO: LOG ERROR!
+                        Console.WriteLine("ERROR! " + e.Message);
+                        response = "";   
+                    } 
+                    finally 
+                    {   
+                        //SENDING RESPONSE BACK TO SENDER: 
+                        var responseBytes = Encoding.UTF8.GetBytes(response);
+                        channel.BasicPublish(exchange: "amq.direct", routingKey: "Search", basicProperties: replyProps, body: responseBytes);
+                        channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+                    }
+                }; 
+            } 
+        }
+
+        private string Make_Request_And_Get_Response(string message)
+        {
+            return null; 
         }
     }
 }
