a09eb552ced52559dcaacea5b8737fb475f1f0a4
Peanguin <Peanguin@users.noreply.github.com>
2022-06-21 15:08:39 +0200

    Start of implementing consume

--- Backend/Kulturarv_Final_Project_PBA/SearchService/api/rabbitmq/rpc_search_handler/Search_Rpc.cs
+++ Backend/Kulturarv_Final_Project_PBA/SearchService/api/rabbitmq/rpc_search_handler/Search_Rpc.cs
@@ -3,59 +3,84 @@ using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using System.Text;
 using Microsoft.Extensions.Configuration;
+using System.Collections.Concurrent;
 
 namespace rabbitmq.rpc_search_handler
 {
-<<<<<<< HEAD:Backend/Kulturarv_Final_Project_PBA/SearchService/rabbitmq/rpc_search_handler/Search_Rpc.cs
-public class RPCServer
+public class Search_Rpc
 {
     private readonly IConfiguration _config;
-    public RPCServer(IConfiguration config)
-    {
-        _config = config;
-		while(true)
-        Listen_And_Respond();
-    }
-=======
 	private readonly IConnection connection;
     private readonly IModel channel;
     private string replyQueueName;
     private EventingBasicConsumer consumer;
     private readonly BlockingCollection<string> respQueue = new BlockingCollection<string>();
     private IBasicProperties props;
-	public Search_Rpc()
+	public Search_Rpc(IConfiguration config)
 	{
-		Init(); 
-	}
-
-	//TODO: Call the service. 
-	
-	private void Init()
-	{
-		Console.WriteLine("Search_Rpc init!");
-		var factory = new ConnectionFactory();	//TODO: --> Need URL.
-		using(var connection = factory.CreateConnection())
-		{
-			using(var channel = connection.CreateModel())
+			ConnectionFactory factory = new ConnectionFactory();
+			factory.UserName = "qrnmzmvv";
+			factory.Password = "acgnVEp0crbkjhO_iySLLAxSFV-CiK3J";
+			factory.Port = 5672;
+			factory.VirtualHost = "qrnmzmvv";
+			factory.HostName = "hawk.rmq.cloudamqp.com";
+			connection = factory.CreateConnection();
+			channel = connection.CreateModel();
+			replyQueueName = channel.QueueDeclare().QueueName;
+			consumer = new EventingBasicConsumer(channel);
+			//Event when it receives message
+			consumer.Received += (model, ea) =>
 			{
-				replyQueueName = channel.QueueDeclare().QueueName; 
-				consumer = new EventingBasicConsumer(channel);
+				var body = ea.Body.ToArray();
+				var message = Encoding.UTF8.GetString(body);
+			};
+			
+			
+			//Consumes from channel declared in the queue name
+			channel.BasicConsume
+			(
+				queue: "RPC_Request_From_Aggregator",
+				autoAck: true,
+				consumer: consumer
+			) ; ;
+			
+			//Init(); 
+		}
 
-				props = channel.CreateBasicProperties();
-        		var correlationId = Guid.NewGuid().ToString();
-        		props.CorrelationId = correlationId;
-        		props.ReplyTo = replyQueueName;
+		//TODO: Call the service. 
 
-				consumer.Received += (model, ea) =>
-        	{
-            	var body = ea.Body.ToArray();
-            	var response = Encoding.UTF8.GetString(body);
-            	if (ea.BasicProperties.CorrelationId == correlationId)
-            	{
-                	respQueue.Add(response);
-            	}
-        	};
->>>>>>> developer:Backend/Kulturarv_Final_Project_PBA/SearchService/api/rabbitmq/rpc_search_handler/Search_Rpc.cs
+		private void Init()
+		{
+			Console.WriteLine("Search_Rpc init!");
+			var factory = new ConnectionFactory();  //TODO: --> Need URL.
+			factory.UserName = "qrnmzmvv";
+			factory.VirtualHost = "qrnmzmvv";
+			factory.Password = "acgnVEp0crbkjhO_iySLLAxSFV-CiK3J";
+			factory.HostName = "hawk.rmq.cloudamqp.com";
+			using (var connection = factory.CreateConnection())
+			{
+				using (var channel = connection.CreateModel())
+				{
+					replyQueueName = channel.QueueDeclare().QueueName;
+					consumer = new EventingBasicConsumer(channel);
+
+					props = channel.CreateBasicProperties();
+					var correlationId = Guid.NewGuid().ToString();
+					props.CorrelationId = correlationId;
+					props.ReplyTo = replyQueueName;
+
+					consumer.Received += (model, ea) =>
+				{
+					var body = ea.Body.ToArray();
+					var response = Encoding.UTF8.GetString(body);
+					if (ea.BasicProperties.CorrelationId == correlationId)
+					{
+						respQueue.Add(response);
+					}
+				};
+				}
+			}
+		}
 
     private ConnectionFactory Setup_Connection_Factory()
     {
@@ -70,52 +95,7 @@ public class RPCServer
 		};
         return factory;
     }
-    public void Listen_And_Respond()
-    {
-        var factory = Setup_Connection_Factory();
-        using (var connection = factory.CreateConnection())
-        using (var channel = connection.CreateModel())
-        {
-            channel.QueueDeclare(queue: "rpc_queue", durable: false,
-              exclusive: false, autoDelete: false, arguments: null);
-            channel.BasicQos(0, 1, false);
-            var consumer = new EventingBasicConsumer(channel);
-            channel.BasicConsume(queue: "rpc_queue",
-              autoAck: false, consumer: consumer);
-            Console.WriteLine(" [x] Awaiting RPC requests");
-
-            consumer.Received += (model, ea) =>
-            {
-                string response = null;
-
-                var body = ea.Body.ToArray();
-                var props = ea.BasicProperties;
-                var replyProps = channel.CreateBasicProperties();
-                replyProps.CorrelationId = props.CorrelationId;
-
-                try
-                {
-                    var message = Encoding.UTF8.GetString(body);
-                    //int n = int.Parse(message);
-                    //Console.WriteLine(" [.] fib({0})", message);
-                    //response = fib(n).ToString(); --> Method of our choice. 
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(" [.] " + e.Message);
-                    response = "awds";
-                }
-                finally
-                {
-                    var responseBytes = Encoding.UTF8.GetBytes(response);
-                    channel.BasicPublish(exchange: "", routingKey: props.ReplyTo,
-                      basicProperties: replyProps, body: responseBytes);
-                    channel.BasicAck(deliveryTag: ea.DeliveryTag,
-                      multiple: false);
-                }
-            };
-        }
-    }
+    
 
     private void Check_Message_Body()
     {
